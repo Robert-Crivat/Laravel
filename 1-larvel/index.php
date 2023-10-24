6703a03e42a4654bd1960c59de82111f7baa4728
Robert-Crivat <72152687+Robert-Crivat@users.noreply.github.com>
2023-10-24 09:41:19 +0200

    1-laravel
    1-laravel commit

---
+++ 1-larvel/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>demo</title>
+</head>
+    <body>
+        
+        <h1>
+
+        <?php 
+            echo "Hello, world";
+        ?>
+
+        </h1>
+
+    </body>
+</html>
