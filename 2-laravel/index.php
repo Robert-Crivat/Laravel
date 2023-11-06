d793ecb7a301568c4b41ab06d72c4d369c26eb12
Robert-Crivat <72152687+Robert-Crivat@users.noreply.github.com>
2023-11-06 12:42:14 +0100

    up
    up

--- 2-laravel/index.php
+++ 2-laravel/index.php
@@ -6,14 +6,13 @@ require "functions.php";
 
 $dsn= 'mysql:host=127.0.0.1; dbname=Myapp; port = 3306;';
 $db_user = 'root';
-$db_pass = NULL;
 
-$pdo = new PDO($dsn, $db_user, $db_pass);
+$pdo = new PDO($dsn, $db_user,);
 
 $statement = $pdo->prepare("SELECT * from posts");
 $statement->execute();
 $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($Posts as $post) {
+foreach ($posts as $post) {
     echo "<li>". $post['title'] . "</li>";
 }
